2e92ff15f09612cfc9c052c8664f0325692ca14b
Yashasvi <kparjun12@gmail.com>
2023-09-28 16:33:09 +0530

    Frontend + backend in progress payment method issue need a credit card.

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -18,7 +18,8 @@ router.route('/').post(async (req, res) => {
     try {
        const{prompt} =req.body;
 
-       const aiResponse = await openai.createImg({
+       const aiResponse = await openai.images.generate({
+            prompt,
             n: 1,
             size: '1024x1024',
             response_format: 'b64_json',
