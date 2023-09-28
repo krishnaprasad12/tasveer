e24742943c0ed33aa50e50e415bf70da3b5526b9
Yashasvi <kparjun12@gmail.com>
2023-09-28 17:11:07 +0530

    API is working need to use credits carefully so image should be good

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -11,7 +11,7 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY 
   });
 router.route('/').get((req, res) => {
-    res.send('Hello from dalle routes');
+    res.status(200).json({ message: 'Hello from DALL-E!' });
 })
 
 router.route('/').post(async (req, res) => {
@@ -25,11 +25,11 @@ router.route('/').post(async (req, res) => {
             response_format: 'b64_json',
         });
 
-        const image = aiResponse.data.data[0].b64_json;
+        const image = aiResponse.data.b64_json;
         res.status(200).json({photo: image});
     } catch (error) {
-        console.log(error);
-        res.status(500).send(error?.response.data.error.message);
-    }
+        console.error(error);
+        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+      }
 })
 export default router;
