c4ebee28c2670697a64390621e90b587e53b60bc
Yashasvi <kparjun12@gmail.com>
2023-09-28 15:15:02 +0530

    MongoDB part of Backend completed.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './mongodb/connect.js';
+
 
 dotenv.config();
 
@@ -14,9 +16,14 @@ app.get('/', (req, res) => {
 });
 
 const startServer = async () => {
-    app.listen(8080, () => {
-        console.log('Server listening on port http://localhost:8080');
-    });
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => {
+            console.log('Server listening on port http://localhost:8080');
+        });
+    } catch (error) {
+        console.log(error);   
+    }
 };
 
 startServer();
