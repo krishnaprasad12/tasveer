44e6194e5e76c5c392cc26126cbe6cf0c2245570
Yashasvi <kparjun12@gmail.com>
2023-10-01 23:23:55 +0530

    90% project done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,6 +16,9 @@ const Home = () => {
   const [loading,setLoading] = useState(false);
   const [allPosts,setAllPosts] = useState(null);
   const [searchText,setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState(null);
+
 
     const fetchPosts = async() => {
       setLoading(true);
@@ -41,6 +44,18 @@ const Home = () => {
     fetchPosts();
   }, []);
 
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        setSearchedResults(searchResult);
+      }, 500),
+    );
+  };
+
   return (
         <section className='max-w-7xl mx-auto'>
           <div>
@@ -51,7 +66,14 @@ const Home = () => {
           </div>
 
           <div className='mt-16'>
-            <FormField />
+          <FormField
+            LabelName="Search posts"
+            type="text"
+            name="text"
+            placeholder="Search something..."
+            value={searchText}
+            handleChange={handleSearchChange}
+        />
           </div>
 
           <div className='mt-10'>
@@ -68,7 +90,7 @@ const Home = () => {
                 )}
                 <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
                   {searchText ? (
-                    <RenderCards data={[]} title ="No search results found" />
+                    <RenderCards data={searchedResults} title ="No search results found" />
                   ):(
                     <RenderCards data={allPosts}
                       title = "No posts found"
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -29,8 +29,6 @@ router.route('/').post(async (req, res) => {
     try {
       const { name, prompt, photo } = req.body;
       const photoUrl = await cloudinary.uploader.upload(photo);
-      console.log(photoUrl.url);
-  
       const newPost = await Post.create({
         name,
         prompt,
